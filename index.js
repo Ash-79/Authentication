0f2d9a51fe3a25e73771366bcaf11dfb0d681ae1
Akash Roy <akashroy.net@gmail.com>
2023-08-07 15:47:00 +0530

    3rd Commit - Setup the viewengine using ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes/index'));
 
+// setup the view engine ejs
+app.set('viewengine', 'ejs');
+app.set('path', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server : ${port}`);
